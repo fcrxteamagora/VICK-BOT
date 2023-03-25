23c3a0fcefb7122b0625095adb2de56232b3b8a0
MR AGORA <106959345+MR-AGORA@users.noreply.github.com>
2023-03-26 04:25:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,12 +32,7 @@ async def is_admins(chat_id: int):
 
 @bot.on_message(filters.command("start"))
 async def start(client, message):
-        await message.reply_text("Hi! My name is Harry. I'm am AI ROBOT of @DO_JISM_EK_JAAN_OP 𝗛𝝙𝗥𝗥𝗬 ✘ 𝗣𝗢𝗧𝗧𝗘𝗥
-┏━━━━━━━━━━━━━❥
-┣ ᴏᴡɴᴇʀ​  @MR_AGORA
-┣ sᴜᴘᴘᴏʀᴛ​  @KANNADIGAXD
-┣ ᴜᴘᴅᴀᴛᴇs @ABOUTAGORA
-┗━━━━━━━━━━━━━❥ \n /chatbot - [on|off]")
+        await message.reply_text("Hi! My name is Harry.I am AI Robot Made by @Mr_Agora\n /chatbot - [on|off]")
 
 
 @bot.on_message(
